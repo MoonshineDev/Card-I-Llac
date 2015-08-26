b09ddc9936d04020679c63a5c022fafd3b18834e
Ted Månsken <ted.wik@gmail.com>
2015-08-26 20:50:41 +0200

    Added CardGameEngine project.

---
+++ Test/TddTests/CardGameTests.cs
@@ -0,0 +1,16 @@
+﻿using CardGameEngine;
+using NUnit.Framework;
+
+namespace TddTests
+{
+    [TestFixture]
+    public class CardGameTests
+    {
+        [Test]
+        public void Ctor()
+        {
+            var sut = new CardGame();
+            Assert.IsNotNull(sut);
+        }
+    }
+}
